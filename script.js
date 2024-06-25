9efc5b9c90196808a3ad43bb2f96f0219c68ce39
nabihakhan6t4 <khannabiha923@gmail.com>
2024-06-25 16:33:49 +0300

    start word guessing game completed html and css and start javascript

---
+++ script.js
@@ -0,0 +1,14 @@
+const options = {
+  aroma: "Pleasing smell",
+  pepper: "Salt's partner",
+  halt: "Put a stop to",
+  jump: "Risa suddenly",
+  shuffle: "Mix cards up",
+  combine: "Add; Mix",
+  chaos: "Total disorder",
+  labyrinth: "Maze",
+  disturb: "Interrupt; Upset",
+  shift: "Move; Period of word",
+  machine: "device or appliances",
+};
+const messege = document.getElementById()
